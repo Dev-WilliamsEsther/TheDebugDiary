e1396fe51f6847aacf8f5ce735933ea8a8c92136
Dev-WilliamsEsther <williexther20@gmail.com>
2025-05-16 02:45:51 +0100

    first commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='w-full h-40 bg-cyan-200'>Header</div>
+    <div className='w-full h-20 bg-green-200 flex items-center justify-around'>
+      <div className='w-40 h-[60%] flex justify-center items-center'>
+        <img src="images/trylogo-removebg-preview.png" alt="" className='w-14' />
+        <h3 className='text-[14px] font-medium '>The Debug Diary</h3>
+      </div>
+      <div className='w-[50%] h-[60%] flex justify-around items-center'>
+        <ul className='flex gap-[14px]'>
+          <li>HOME</li>
+          <li>MUSING</li>
+          <li>SELFCARE</li>
+          <li>BEAUTY</li>
+          <li>CAREER</li>
+          <li>RELATIONSHIPS</li>
+          <li>FOOD & RECIPES</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 

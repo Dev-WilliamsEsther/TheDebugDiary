0a062aa8e2ba6d22868f7f973f0fdb78ad276f6f
Dev-WilliamsEsther <williexther20@gmail.com>
2025-05-18 23:06:06 +0100

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full h-80 bg-fuchsia-300'>Footer</div>
+    <div className='w-full h-90 bg-fuchsia-300 flex justify-around'>
+      <div className='w-50 h-60 bg-lime-200'></div>
+      <div className='w-50 h-60 bg-lime-200'></div>
+      <div className='w-50 h-60 bg-lime-200'></div>
+    </div>
   )
 }
 
 export default Footer
+
+
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,25 +1,78 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import { FiX } from 'react-icons/fi';
+import { PiTextAlignRight } from 'react-icons/pi';
 
 const Header = () => {
+  const menuRef = useRef(null);
+  const [underlineStyle, setUnderlineStyle] = useState({ left: 0, width: 0, opacity: 0 });
+
+  const handleMouseEnter = (e) => {
+    const menuRect = menuRef.current.getBoundingClientRect();
+    const linkRect = e.target.getBoundingClientRect();
+    setUnderlineStyle({
+      left: linkRect.left - menuRect.left,
+      width: linkRect.width,
+      opacity: 1,
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setUnderlineStyle((prev) => ({ ...prev, opacity: 0 }));
+  };
+
+  const [mobileMenu,setMobileMenu] = useState(false)
+
+  const toggleMenu = () => {
+    setMobileMenu( prev => !prev)
+  }
+
+  const navItems = ['HOME', 'MUSING', 'SELFCARE', 'BEAUTY', 'CAREER', 'RELATIONSHIPS', 'FOOD & RECIPES'];
+
   return (
     <div className='w-full h-20 bg-green-200 flex items-center justify-around'>
-      <div className='w-40 h-[60%] flex justify-center items-center'>
+      <div className='w-40 h-[60%] bg-amber-500 flex justify-center items-center'>
         <img src="images/trylogo-removebg-preview.png" alt="" className='w-14' />
-        <h3 className='text-[14px] font-medium '>The Debug Diary</h3>
+        <h3 className='text-[14px] font-medium'>The Debug Diary</h3>
       </div>
-      <div className='w-[50%] h-[60%] flex justify-around items-center'>
-        <ul className='flex gap-[14px]'>
-          <li>HOME</li>
-          <li>MUSING</li>
-          <li>SELFCARE</li>
-          <li>BEAUTY</li>
-          <li>CAREER</li>
-          <li>RELATIONSHIPS</li>
-          <li>FOOD & RECIPES</li>
-        </ul>
-      </div>
-    </div>
-  )
-}
 
-export default Header
+      <div className='md:hidden cursor-pointer' onClick={toggleMenu}>
+        {mobileMenu ? <FiX  size={24}/> : <PiTextAlignRight size={24} />}
+      </div>
+
+      <div className=' hidden md:flex w-[50%] h-[60%] items-center relative' ref={menuRef}>
+        <ul className='flex gap-[14px] ' onMouseLeave={handleMouseLeave}>
+          {navItems.map((item, index) => (
+            <li
+              key={index}
+              className='relative cursor-pointer px-1'
+              onMouseEnter={handleMouseEnter}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+
+        {/* Sliding underline */}
+        <div
+          className=' absolute bottom-0 h-[2px] bg-black transition-all duration-300 ease-in-out'
+          style={{
+            left: underlineStyle.left,
+            width: underlineStyle.width,
+            opacity: underlineStyle.opacity,
+          }}
+        />
+      </div>
+      {mobileMenu && (
+        <ul className='absolute top-20 right-4 w-[70vw] h-120 bg-lime-700 rounded shadow-lg p-4 flex flex-col gap-3 z-50 md:hidden'>
+        {navItems.map((item, index) => (
+          <li key={index} className='cursor-pointer'>
+            {item}
+          </li>
+        ))}
+      </ul>
+      )}
+    </div>
+  );
+};
+
+export default Header;
